da70b5860be8a37f337e06bdce9df5bbabdb5e0c
tetradog <tetradog@gmail.com>
2018-11-05 06:41:57 +0100

    subir clases

---
+++ Gabriel.Cat.Xamarin.Wpf/Inicializador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF.Extensions;
+
+namespace Gabriel.Cat.Xamarin.Wpf
+{
+    public static class Inicializador<TXamarinFormsMainPage, TWPFMainWindows>
+        where TXamarinFormsMainPage : ContentPage, new()
+        where TWPFMainWindows : Window  
+
+    {
+        public static void Start(System.Windows.Application currentApp, TWPFMainWindows mainWpf)
+        {
+            TXamarinFormsMainPage main;
+            Forms.Init();
+            main = new TXamarinFormsMainPage();
+            if (main.Title != null)
+                mainWpf.Title = main.Title;
+            currentApp.MainWindow.Content = main.ToFrameworkElement();
+        }
+    }
+}
